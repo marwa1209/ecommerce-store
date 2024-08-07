6a5428648799f0de27ac125a9425a75cc68decc0
mustafa <mustafamufeed22@gmail.com>
2024-08-07 19:02:43 +0300

    edit logic of showing filters in category

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,31 +1,30 @@
-
-import Container from '@/components/ui/container';
-import Billboard from '@/components/ui/billboard';
-import ProductCard from '@/components/ui/product-card';
-import NoResults from '@/components/ui/no-results';
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
 
 import { getCategory, getColors, getProducts, getSizes } from "@/data";
 
-import Filter from '@/components/category/filter';
-import MobileFilters from '@/components/category/mobile-filters';
+import Filter from "@/components/category/filter";
+import MobileFilters from "@/components/category/mobile-filters";
 
 export const revalidate = 0;
 
 interface CategoryPageProps {
   params: {
     categoryId: string;
-  },
+  };
   searchParams: {
     colorId: string;
     sizeId: string;
-  }
+  };
 }
 
-const CategoryPage: React.FC<CategoryPageProps> = async ({ 
-  params, 
-  searchParams
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+  params,
+  searchParams,
 }) => {
-  const products = await getProducts({ 
+  const products = await getProducts({
     categoryId: params.categoryId,
     colorId: searchParams.colorId,
     sizeId: searchParams.sizeId,
@@ -34,6 +33,8 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
   const colors = await getColors();
   const category = await getCategory(params.categoryId);
 
+  const dataIsFiltered = !!(searchParams.colorId || searchParams.sizeId);
+
   return (
     <div className="bg-white">
       <Container>
@@ -41,11 +42,18 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
             <MobileFilters sizes={sizes} colors={colors} />
-            {products.length > 0 && (
+            {dataIsFiltered ? (
               <div className="hidden lg:block">
                 <Filter valueKey="sizeId" name="Sizes" data={sizes} />
                 <Filter valueKey="colorId" name="Colors" data={colors} />
               </div>
+            ) : (
+              products.length > 0 && (
+                <div className="hidden lg:block">
+                  <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+                  <Filter valueKey="colorId" name="Colors" data={colors} />
+                </div>
+              )
             )}
             <div className="mt-6 lg:col-span-4 lg:mt-0">
               {products.length === 0 && <NoResults />}
