d69a0191adcdd06f7025d76d13ed6f815a680cef
Marwa Mahmoud <mm3764453@gmail.com>
2024-08-12 18:21:58 +0300

    CU-86bzyh52e - fix, enhancements to side-drawer (admin - store)

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -28,70 +28,79 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
   }));
 
   return (
-    <nav className="mx-6 flex items-center lg:space-x-6">
-      {routes?.map((route) => (
-        <Link
-          key={route.href}
-          href={route.href}
-          className={cn(
-            "text-sm font-medium transition-colors hover:text-primary hidden lg:block",
-            route.active
-              ? "text-black dark:text-white"
-              : "text-muted-foreground"
-          )}
-        >
-          {route.label}
-        </Link>
-      ))}
-      <div className="flex items-center justify-between w-full lg:hidden">
-        <button
-          onClick={toggleMenu}
-          className="text-gray-500 hover:text-gray-700 focus:outline-none"
-        >
-          {menuOpen ? "" : <MenuIcon className="w-6 h-6" />}
-        </button>
-      </div>
-      <div
-        className={cn(
-          "fixed inset-y-0 left-0 z-30 w-64 transform bg-white shadow-lg transition-all duration-500 ease-in-out lg:hidden",
-          {
-            "-translate-x-full": !menuOpen,
-            "translate-x-0": menuOpen,
-          }
-        )}
-      >
-        <div className="flex items-center justify-between p-4">
-          <span className="text-xl font-semibold">Menu</span>
+    <>
+      {menuOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-20"
+          onClick={() => setMenuOpen(false)}
+        ></div>
+      )}
+      <nav className="mx-6 flex items-center lg:space-x-6">
+        {routes?.map((route) => (
+          <Link
+            key={route.href}
+            href={route.href}
+            className={cn(
+              "text-sm font-medium transition-colors hover:text-primary hidden lg:block",
+              route.active
+                ? "text-black dark:text-white"
+                : "text-muted-foreground"
+            )}
+          >
+            {route.label}
+          </Link>
+        ))}
+        <div className="flex items-center justify-between w-full lg:hidden">
           <button
             onClick={toggleMenu}
             className="text-gray-500 hover:text-gray-700 focus:outline-none"
           >
-            <XIcon className="w-6 h-6" />
+            {menuOpen ? "" : <MenuIcon className="w-6 h-6" />}
           </button>
         </div>
-        <div className="overflow-y-auto h-full">
-          <div className="flex flex-col space-y-2 p-4">
-            {routes?.map((route) => {
-              const isActive = pathname === route.href;
-              return (
-                <Link
-                  key={route.href}
-                  href={route.href}
-                  className={cn(
-                    "block px-4 py-2 text-sm font-medium transition-colors hover:text-primary",
-                    isActive
-                      ? "text-black dark:text-white"
-                      : "text-muted-foreground"
-                  )}
-                >
-                  {route.label}
-                </Link>
-              );
-            })}
+        <div
+          className={cn(
+            "fixed inset-y-0 left-0 z-30 w-64 transform bg-white shadow-lg transition-all duration-500 ease-in-out lg:hidden",
+            {
+              "-translate-x-full": !menuOpen,
+              "translate-x-0": menuOpen,
+            }
+          )}
+        >
+          <div className="flex items-center justify-between p-4">
+            <span className="text-xl font-semibold">Menu</span>
+            <button
+              onClick={toggleMenu}
+              className="text-gray-500 hover:text-gray-700 focus:outline-none"
+            >
+              <XIcon className="w-6 h-6" />
+            </button>
+          </div>
+          <div className="overflow-y-auto h-full ">
+            <div className="flex flex-col space-y-2 p-4">
+              {routes?.map((route) => {
+                const isActive = pathname === route.href;
+                return (
+                  <Link
+                    key={route.href}
+                    href={route.href}
+                    onClick={() => setMenuOpen(false)}
+                    className={cn(
+                      "block px-4 py-2 text-sm font-medium transition-colors hover:text-primary",
+                      isActive
+                        ? "text-black dark:text-white"
+                        : "text-muted-foreground"
+                    )}
+                  >
+                    {route.label}
+                  </Link>
+                );
+              })}
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
